54b16fa353d99e8afac0da2b869a242f525f94a3
madkingcabbage <kibibyt3@proton.me>
2023-09-28 22:01:00 -0400

    removed pre-rewrite files from the rewrite repo

--- chat.h
+++
@@ -1,9 +0,0 @@
-#define MESSAGE_LENGTH  400
-#define RESPONSE_LENGTH 400
-#define DIRECTOR_LENGTH 400
-#define MAX_RESPONSES   6
-#define MAX_SECTIONS    100
-
-int chatinit(char*);
-int chatio(char*, char***);
-command getActiveCommand();
