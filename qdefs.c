aa3e3c404a9ca7b30e32dc27c87e365ac700e00c
kibibyte <kibibyt3@proton.me>
2023-10-01 21:35:14 -0400

    added member 'type' to Qdatameta_t and an enum to store the various types

--- qdefs.c
+++ qdefs.c
@@ -11,13 +11,15 @@
 /*
  * Creates a datameta. Size must be the number of elements allocated to the
  * given pointer. data's memory must be allocated in advance and merely cast to
- * the form of Qdata_t*
+ * the form of Qdata_t*. Param type holds the type of data and count holds the
+ * number of type in data.
  */ 
 Qdatameta_t*
-qdatameta_create(Qdata_t* data, size_t size) {
+qdatameta_create(Qdata_t* data, QdataType_t type, size_t count) {
 	Qdatameta_t datameta;
 	datametap = &datameta;
 	datametap->datap = data;
-	datametap->size = size;
+	datametap->count = count;
+	datametap->type = type;
 	return &datameta;
 }
--- qdefs.h
+++ qdefs.h
@@ -8,6 +8,13 @@
 #define Q_OK     0
 #define Q_ERROR -1
 
+typedef enum QdataType_t {
+	QDATA_TYPE_INT = 1,
+	QDATA_TYPE_FLOAT,
+	QDATA_TYPE_STRING,
+	QDATA_TYPE_COUNT = QDATA_TYPE_STRING
+} QdataSpecies_t;
+
 /* 
  * Type for holding arbitrary amounts of data to pass between modules. Meant to
  * be used in a pointer; it should never be called in isolation prior to being
@@ -17,8 +24,9 @@ typedef void Qdata_t;
 
 /* Type for holding Qdata and its size */
 typedef struct Qdatameta_t {
-	QData_t *datap;
-	size_t   size; 
+	Qdata_t    *datap;
+	size_t      count;
+	QdataType_t type;
 } Qdatameta_t;
 
-Qdatameta_t *qdatameta_create(Qdata_t*, size_t);
+Qdatameta_t *qdatameta_create(Qdata_t*, QdataType_t, size_t);
