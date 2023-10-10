71497b6e9927c3fa0feed766e441a1985780cc11
kibibyte <kibibyt3@proton.me>
2023-10-09 21:54:21 -0400

    more work on qwalk

--- include/qwalk.h
+++ include/qwalk.h
@@ -10,13 +10,38 @@
 /** Area of the qwalk field */
 #define QWALK_AREA_SIZE (QWALK_AREA_SIZE_Y * QWALK_AREA_SIZE_X)
 
+
+
+/**
+ * Commands to be processed by the logic of the game.
+ * The entire walk module needs to be know these commands; this is because the
+ * I/O submodule translates raw input into commands.
+ */
+typedef enum QwalkCommand_t {
+	QWALK_COMMAND_MOVE_NORTH = Q_ENUM_VALUE_START, /**< Move north */
+	QWALK_COMMAND_MOVE_EAST,                       /**< Move east  */
+	QWALK_COMMAND_MOVE_SOUTH,                      /**< Move south */
+	QWALK_COMMAND_MOVE_WEST,                       /**< Move west  */
+	/** Pass a tick without taking an action*/
+	QWALK_COMMAND_WAIT,                            
+
+	/**
+	 * Number of possible commands.
+	 * Must be equal to the final enum constant.
+	 */
+	QWALK_COMMAND_COUNT = QWALK_COMMAND_WAIT
+} QwalkCommand_t;
+
+
+
 /**
  * An object in a #QwalkField_t.
  */
 typedef struct QwalkObject_t {
 	int coord_y;                  /**< X coord of object */
 	int coord_x;                  /**< Y coord of object */
-	QattrList_t *attr_list;       /**< #QattrList_t pointer for attributes of object */
+	/** #QattrList_t pointer for object attributes */
+	QattrList_t *attr_list;
 } QwalkObject_t;
 
 /**
@@ -34,14 +59,17 @@ typedef struct QwalkField_t {
 
 } QwalkField_t;
 
+
+
+
 /** Initialize the qwalk module              */
-extern int               qwalk_init(Qdatameta_t *);
+extern           int               qwalk_init(Qdatameta_t *);
 
 /** Safely exit the qwalk module             */
-extern int               qwalk_end(void);
+extern           int               qwalk_end(void);
 
 /** Pass a tick in the qwalk module          */
-extern ModeSwitchData_t *qwalk_tick(void);
+extern /*@null@*/ModeSwitchData_t *qwalk_tick(void);
 
 
 
