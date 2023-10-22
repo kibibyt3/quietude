e245ecd78a55e701219ac27a43e84657383d33d4
kibibyte <kibibyt3@proton.me>
2023-10-22 14:42:54 -0400

    added new errors

--- include/qerror.h
+++ include/qerror.h
@@ -30,46 +30,49 @@
  * Internal error for Q.
  */
 typedef enum Qerror_t {
-	/** Error for unexpected @c NULL pointer.                               */
+	/** Error for unexpected @c NULL pointer.                                 */
 	QERROR_NULL_POINTER_UNEXPECTED = Q_ENUM_VALUE_START,
 	
-	/** Error for unexpected non-`NULL` pointer.                            */
+	/** Error for unexpected non-`NULL` pointer.                              */
 	QERROR_NONNULL_POINTER_UNEXPECTED,
 
-	/** Error for unexpected @c NULL value.                                 */
+	/** Error for unexpected @c NULL value.                                   */
 	QERROR_NULL_VALUE_UNEXPECTED,
 
-	/** Error for an unexpected zero value.                                 */
+	/** Error for an unexpected zero value.                                   */
 	QERROR_ZERO_VALUE_UNEXPECTED,
 
-	/** Error for an unexpected negative value.                             */
+	/** Error for an unexpected negative value.                               */
 	QERROR_NEGATIVE_VALUE_UNEXPECTED,
 
-	/** Error for invalid @c enum constant.                                 */
+	/** Error for invalid @c enum constant.                                   */
 	QERROR_ENUM_CONSTANT_INVALID,
 
-	/** Error for an @c enum constant set to zero.                          */
+	/** Error for an @c enum constant set to zero.                            */
 	QERROR_ENUM_CONSTANT_INVALID_ZERO,
 
-	/** Error for indexing a location that does not exist.                  */
+	/** Error for a @c struct not entirely filled (namely when it should be). */
+	QERROR_STRUCT_INCOMPLETE,
+
+	/** Error for indexing a location that does not exist.                    */
 	QERROR_INDEX_OUTOFRANGE,
 
-	/** Error for an impossible parameter.                                  */
+	/** Error for an impossible parameter.                                    */
 	QERROR_PARAMETER_INVALID,
 
-	/** Error for an already-initialized module.                            */
+	/** Error for an already-initialized module.                              */
 	QERROR_MODULE_INITIALIZED,
 
-	/** Error for an uninitialized module.                                  */
+	/** Error for an uninitialized module.                                    */
 	QERROR_MODULE_UNINITIALIZED,
 
-	/** Error for violating internal file read/write permissions.           */
+	/** Error for violating internal file read/write permissions.             */
 	QERROR_FILE_MODE,
 
-	/** Error for a #Qdatameta_t whose usage is incompatible with its type. */
+	/** Error for a #Qdatameta_t whose usage is incompatible with its type.   */
 	QERROR_QDATAMETA_TYPE_INCOMPATIBLE,
 
-	/** Error for a #Qdatameta_t whose @c count disagrees with its @c type. */
+	/** Error for a #Qdatameta_t whose @c count disagrees with its @c type.   */
 	QERROR_QDATAMETA_TYPE_COUNT_INCOMPATIBLE,
 	
 	/**
