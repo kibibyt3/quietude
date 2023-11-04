180e70ba07f59fe5e96335312ee759170e20d0e4
kibibyte <kibibyt3@proton.me>
2023-11-04 16:45:00 -0400

    ioutils: tweaked docs

--- include/ioutils.h
+++ include/ioutils.h
@@ -9,12 +9,12 @@
  * Type to refer to cursor styles.
  */
 typedef enum CursStyle_t {
-	CURS_STYLE_BLINKING_BLOCK = Q_ENUM_VALUE_START, /** Blinking block. */
-	CURS_STYLE_STEADY_BLOCK, /** Steady block.       */
-	CURS_STYLE_BLINKING_UL,  /** Blinking underline. */
-	CURS_STYLE_STEADY_UL,    /** Steady underline.   */
-	CURS_STYLE_BLINKING_BAR, /** Blinking bar.       */
-	CURS_STYLE_STEADY_BAR,   /** Steady bar.         */
+	CURS_STYLE_BLINKING_BLOCK = Q_ENUM_VALUE_START, /**< Blinking block. */
+	CURS_STYLE_STEADY_BLOCK, /**< Steady block.       */
+	CURS_STYLE_BLINKING_UL,  /**< Blinking underline. */
+	CURS_STYLE_STEADY_UL,    /**< Steady underline.   */
+	CURS_STYLE_BLINKING_BAR, /**< Blinking bar.       */
+	CURS_STYLE_STEADY_BAR,   /**< Steady bar.         */
 
 	/** Number of possible values for a #CursStyle_t. */
 	CURS_STYLE_COUNT = CURS_STYLE_STEADY_BAR
