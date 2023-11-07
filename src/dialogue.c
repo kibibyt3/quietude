130a3390a1b3c7b2a9e5fc6995ca347e742d872c
kibibyte <kibibyt3@proton.me>
2023-11-07 14:31:40 -0500

    complete first half of dialogue init func

--- src/dialogue.c
+++ src/dialogue.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <ctype.h>
+#include <stdbool.h>
 
 #include "qdefs.h"
 #include "qerror.h"
+
+#include "dialogue.h"
 
 
 
@@ -17,47 +21,65 @@ static long file_size_get(FILE *);
  */
 int
 dialogue_init(const char *qdl_filename) {
-	returnval = Q_OK;
+	int returnval = Q_OK;
 	FILE *qdl_file;
 	int ch;
 	long file_size;
 	char *file_string_raw;
 
-	if ((qdl_file = fopen(qdl_file, "r")) == NULL) {
+	if ((qdl_file = fopen(qdl_filename, "r")) == NULL) {
 		Q_ERROR_SYSTEM("fopen()");
 		return Q_ERROR;
 	}
 
-	if ((file_size = file_size_get(fp)) == Q_ERRORCODE_LONG) {
+	if ((file_size = file_size_get(qdl_file)) == Q_ERRORCODE_LONG) {
 		Q_ERRORFOUND(QERROR_ERRORVAL);
+		if (fclose(qdl_file) == EOF) {
+			Q_ERROR_SYSTEM("fclose()");
+		}
 		return Q_ERROR;
 	}
 
 	file_size++; /* for null-terminating char */
 
-	if ((file_string_raw = calloc(file_size, sizeof(*file_string_raw))) == NULL) {
+	if ((file_string_raw = calloc((size_t) file_size, sizeof(*file_string_raw))) 
+			== NULL) {
 		Q_ERROR_SYSTEM("calloc()");
+		if (fclose(qdl_file) == EOF) {
+			Q_ERROR_SYSTEM("fclose()");
+		}
 		return Q_ERROR;
 	}
 
-	index = 0;
+	/* add every non-whitespace character to a string */
+	int index = 0;
 	while ((ch = fgetc(qdl_file)) != EOF) {
 		if (isspace(ch) == 0) { /* that is, if ch is not a whitespace character */
-			file_string_raw[index++] = ch;
+			file_string_raw[index++] = (char) ch;
 		}
 	}
-	file_string_raw[index] = '\0';
-
+	
 	/* check if file error indicator is set */
 	if (ferror(qdl_file) != 0) {
 		Q_ERRORFOUND(QERROR_ERRORVAL);
-		free(file_string_raw);
-		return Q_ERROR;
+		returnval = Q_ERROR;
+	}
+	
+	if (fclose(qdl_file) == EOF) {
+		Q_ERROR_SYSTEM("fclose()");
+		returnval = Q_ERROR;
+	}
+	
+	file_string_raw[index] = '\0';
+
+	/* FIXME: debug code */
+	if (printf("%s", file_string_raw) < 0) {
+		Q_ERROR_SYSTEM("printf()");
+		returnval = Q_ERROR;
 	}
 
-	printf("%s", file_string_raw);
-
-	return Q_OK;
+	free(file_string_raw);
+	return returnval;
 }
 
 
