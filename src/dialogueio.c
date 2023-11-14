54590b639a99bcb9eb81f496c4f625b47958076f
kibibyte <kibibyt3@proton.me>
2023-11-14 13:57:38 -0500

    create dialogueio and add to Makefile

---
+++ src/dialogueio.c
@@ -0,0 +1,12 @@
+/**
+ * @file dialogueio.c
+ * Program file for the I/O half of NPC dialogue and related functions.
+ */
+
+
+
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include "qdefs.h"
+#include "qerror.h"
