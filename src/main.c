9b712f792e53290a6659076dc581017f96123b4d
kibibyte <kibibyt3@proton.me>
2023-11-21 14:50:23 -0500

    tweak main.c

--- src/main.c
+++ src/main.c
@@ -3,9 +3,17 @@
  * Main file for Q.
  */
 
+
+
+#include <stdlib.h>
+#include <stdbool.h>
+
 #include "qdefs.h"
+#include "qerror.h"
 
 #include "mode.h"
+
+
 
 /**
  * Main function of Q.
@@ -15,18 +23,22 @@
  */
 int main(int argc, char** argv) {
 	
-	/* Initializations */
-	assert(mode_init() == Q_OK);
+	if (mode_init() == Q_ERROR) {
+		Q_ERRORFOUND(QERROR_ERRORVAL);
+		exit(EXIT_FAILURE);
+	}
 
 	ModeSwitchData_t  mode_switch_data_next = {MODE_T_INIT, NULL};
 	ModeSwitchData_t *mode_switch_data_nextptr = &mode_switch_data_next;
 	Mode_t mode_curr = MODE_T_INIT;
 
-	while (mode_switch_data_next.mode != MODE_EXIT) {
-		mode_switch(mode_switch_data_next);
-		mode_curr = mode_switch_data_next->mode;
-		while (mode_switch_data_next->mode == mode_curr) {
-			mode_tick(mode_switch_data_next);
+	while (mode_switch_data_next.mode != MODE_T_EXIT) {
+		mode_switch(mode_switch_data_nextptr);
+		mode_curr = mode_switch_data_nextptr->mode;
+		while (mode_switch_data_nextptr->mode == mode_curr) {
+			mode_tick(mode_switch_data_nextptr);
 		}
 	}
+
+	exit(EXIT_SUCCESS);
 }
