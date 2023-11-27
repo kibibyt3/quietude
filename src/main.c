e55404aa3da0ccc523600a399a40e176e594bfde
kibibyte <kibibyt3@proton.me>
2023-11-27 16:19:30 -0500

    complete rough draft of main rewrite

--- src/main.c
+++ src/main.c
@@ -6,11 +6,13 @@
 
 
 #include <stdlib.h>
+#include <stdio.h>
 #include <stdbool.h>
 
 #include "qdefs.h"
 #include "qerror.h"
 
+#include "qattr.h"
 #include "mode.h"
 
 
@@ -28,15 +30,49 @@ int main(int argc, char** argv) {
 		exit(EXIT_FAILURE);
 	}
 
-	ModeSwitchData_t  mode_switch_data_next = {MODE_T_INIT, NULL};
-	ModeSwitchData_t *mode_switch_data_nextptr = &mode_switch_data_next;
-	Mode_t mode_curr = MODE_T_INIT;
+	int *switch_init_data;
 
-	while (mode_switch_data_next.mode != MODE_T_EXIT) {
-		mode_switch(mode_switch_data_nextptr);
-		mode_curr = mode_switch_data_nextptr->mode;
-		while (mode_switch_data_nextptr->mode == mode_curr) {
-			mode_tick(mode_switch_data_nextptr);
+	if ((switch_init_data = calloc((size_t) 1, sizeof(*switch_init_data)))
+			== NULL) {
+		Q_ERROR_SYSTEM("calloc()");
+		exit(EXIT_FAILURE);
+	}
+
+	*switch_init_data = 0;
+
+	Qdatameta_t *switch_init_datameta;
+
+	if ((switch_init_datameta = qdatameta_create(
+					(Qdata_t *) switch_init_data, QDATA_TYPE_INT, (size_t) 1)) == NULL) {
+		Q_ERROR_SYSTEM("calloc()");
+		exit(EXIT_FAILURE);
+	}
+
+	if (mode_buffer_switch(MODE_T_WALK, switch_init_datameta) == Q_ERROR) {
+		Q_ERRORFOUND(QERROR_ERRORVAL);
+	}
+
+	if (mode_switch() == Q_ERROR) {
+		Q_ERRORFOUND(QERROR_ERRORVAL);
+	}
+
+	Mode_t mode_curr;
+
+	while (true) {
+		if (mode_switch() == Q_ERROR) {
+			Q_ERRORFOUND(QERROR_ERRORVAL);
+		}
+
+		/* check if we should exit */
+		if ((mode_curr = mode_curr_get()) == (Mode_t) Q_ERRORCODE_ENUM) {
+			Q_ERRORFOUND(QERROR_ERRORVAL);
+		}
+		if (mode_curr == MODE_T_EXIT) {
+			break;
+		}
+
+		if (mode_tick() == Q_ERROR) {
+			Q_ERRORFOUND(QERROR_ERRORVAL);
 		}
 	}
 
