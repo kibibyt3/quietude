0e8f150b10537bce22b66aea4455c24eaec08c06
kibibyte <kibibyt3@proton.me>
2023-12-11 15:25:46 -0500

    add typedefs to player module

--- include/player.h
+++ include/player.h
@@ -6,9 +6,19 @@
 
 
 
-typedef struct Player_t {
+typedef struct Agent_t {
 	char *name;
-	Qflags_t flags;
 	int hp;
 	int level;
+	Qflags_t status_flags;
+} Agent_t;
+
+typedef struct Player_t {
+	Agent_t agent;
+	Qflags_t progress_flags;
 } Player_t;
+
+typedef struct AgentMain_t {
+	Agent_t agent;
+	Qflags_t tree_flags;
+}
