149009bb103664ce26fd2f39a1f984ea173bfe5c
kibibyte <kibibyt3@proton.me>
2023-12-21 16:42:24 -0500

    begin writing interface for agent

--- include/agent.h
+++ include/agent.h
@@ -1,8 +1,9 @@
 /**
  * @file agent.h
  * Header file for the agent module.
- * Depends on @ref qdefs.h.
+ * Depends on @ref qdefs.h and @ref item.h.
  */
+
 
 
 
@@ -20,6 +21,8 @@ typedef enum AgentType_t {
 
 } AgentType_t;
 
+
+#define AGENT_NAME_LEN_MAX	30
 typedef struct Agent_t {
 	AgentType_t agent_type;
 	char *name;
@@ -34,6 +37,23 @@ typedef struct Agent_t {
 
 
 /*@only@*//*@null@*/
-extern Agent_t *agent_create(int preset_index);
+extern Agent_t	*agent_create(char *preset_file);
+extern void			agent_destroy(/*@only@*/Agent_t *agent);
 
-extern void agent_destroy(/*@only@*/Agent_t *agent);
+
+extern AgentType_t	agent_type_get(Agent_t *agent);
+extern char					*agent_name_get(Agent_t *agent);
+extern int					agent_hp_max_get(Agent_t *agent);
+extern int					agent_hp_get(Agent_t *agent);
+extern int					agent_level_get(Agent_t *agent);
+extern ItemID_t			agent_inventory_item_id_get(Agent_t *agent, int index);
+extern bool					agent_flags_getf(int i, Agent_t *agent);
+
+
+extern int	agent_type_set(Agent_t *agent, AgentType_t agent_type);
+extern int	agent_name_set(Agent_t *agent, char *name);
+extern void	agent_hp_max_set(Agent_t *agent, int hp_max);
+extern void	agent_hp_set(Agent_t *agent, int hp);
+extern void	agent_level_set(Agent_t *agent, int level);
+extern int	agent_inventory_item_id_set(Agent_t *agent, int index, ItemID_t id);
+extern int	agent_flags_setf(int i, Agent_t *agent, bool b);
--- src/agent.c
+++ src/agent.c
@@ -10,3 +10,137 @@
 
 #include "qdefs.h"
 #include "qerror.h"
+
+
+
+
+
+Agent_t *
+agent_create(char *preset_file) {
+	return NULL;
+}
+
+
+void
+agent_destroy(/*@only@*/Agent_t *agent) {
+	return;
+}
+
+
+
+int
+agent_type_set(Agent_t *agent, AgentType_t agent_type) {
+
+	if ((agent_type < (AgentType_t) Q_ENUM_VALUE_START)
+			|| (agent_type > AGENT_TYPE_COUNT)) {
+		Q_ERRORFOUND(QERROR_ENUM_CONSTANT_INVALID);
+		return Q_ERROR;
+	}
+
+	agent->agent_type = agent_type;
+
+	return Q_OK;
+}
+
+
+int
+agent_name_set(Agent_t *agent, char *name) {
+
+	if (strlen(name) > (size_t) AGENT_NAME_LEN_MAX) {
+		Q_ERRORFOUND(QERROR_PARAMETER_INVALID);
+		return Q_ERROR;
+	}
+
+	agent->name = name;
+	return Q_OK;
+}
+
+
+void
+agent_hp_max_set(Agent_t *agent, int hp_max) {
+
+	agent->hp_max = hp_max;
+	return;
+}
+
+
+void
+agent_hp_set(Agent_t *agent, int hp) {
+
+	agent->hp = hp;
+	return;
+}
+
+
+void
+agent_level_set(Agent_t *agent, int level) {
+
+	agent->level = level;
+	return;
+}
+
+
+int
+agent_inventory_item_id_set(Agent_t *agent, int index, ItemID_t id) {
+
+	if (item_inventory_item_set(agent->inventory, index, id) == Q_ERROR) {
+		Q_ERRORFOUND(QERROR_ERRORVAL);
+		return Q_ERROR;
+	}
+
+	return Q_OK;
+}
+
+
+void
+agent_flags_setf(int i, Agent_t *agent, bool b) {
+
+	qflags_setf(i, agent->flags, b);
+	return;
+}
+
+
+
+AgentType_t
+agent_type_get(Agent_t *agent) {
+	return agent->agent_type;
+}
+
+
+char *
+agent_name_get(Agent_t *agent) {
+	return agent->name;
+}
+
+
+int
+agent_hp_max_get(Agent_t *agent) {
+	return agent->hp_max;
+}
+
+
+int
+agent_hp_get(Agent_t *agent) {
+	return agent->hp;
+}
+
+
+int
+agent_level_get(Agent_t *agent) {
+	return agent->level;
+}
+
+
+ItemID_t
+agent_inventory_item_id_get(Agent_t *agent, int index) {
+
+}
+
+
+bool
+agent_flags_getf(int i, Agent_t *agent) {
+
+}
+
+
+
